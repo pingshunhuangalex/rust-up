c9543c8b2b46fece4b2584f96ff4b4413f8adecb
Pingshun Huang <ahuang@atlassian.com>
2022-09-23 17:29:31 +1000

    Add lyrics

---
+++ lyrics/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    const ORDINAL_NUMS: [&str; 12] = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"
+    ];
+    const PATTERNS: [&str; 12] = [
+        "a partridge in a pear tree.",
+        "two turtle doves,",
+        "three french hens,",
+        "four calling birds,",
+        "five gold rings,",
+        "six geese a-laying,",
+        "seven swans a-swimming,",
+        "eight maids a-milking,",
+        "nine ladies dancing,",
+        "ten lords a-leaping,",
+        "eleven pipers piping,",
+        "twelve drummers drumming,",
+    ];
+
+    for n in 0..ORDINAL_NUMS.len() {
+        println!("\nOn the {} day of Christmas my true love sent to me,", ORDINAL_NUMS[n]);
+
+        for m in (0..n + 1).rev() {
+            if m == 0 && n > 0 {
+                println!("and {}", PATTERNS[m].to_lowercase());
+            } else {
+                println!("{}", PATTERNS[m]);
+            }
+        }
+    }
+}
